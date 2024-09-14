3ee3ca8341424707b9c6d76fe55de9cb9f29f134
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-09-14 12:02:53 +0700

    #1 Code base

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+// const dbConnect = require("./config/dbconnect")
+// const initRoutes = require("./routes")
+// const cookieParser = require("cookie-parser")
+// const cors = require("cors")
+
+const app = express();
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     methods: ["POST", "PUT", "GET", "DELETE"],
+//     credentials: true,
+//   })
+// )
+// app.use(cookieParser())
+const port = process.env.PORT || 8888;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// dbConnect()
+// initRoutes(app)
+
+app.use("/", (req, res) => {
+  res.send("SERVER ONNNN");
+});
+
+app.listen(port, () => {
+  console.log("Server running on the port: " + port);
+});
