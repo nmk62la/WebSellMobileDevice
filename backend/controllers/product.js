2e4e96e294cb389dafb11cb79a3569e7d98ad029
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-09-19 06:05:56 +0700

    #4 Filter + Sort + Page cho product & CRUD category & CRUD blog

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -21,11 +21,68 @@ const getProduct = asyncHandler(async (req, res) => {
 });
 // Filtering, sorting & pagination
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
+  const queries = { ...req.query };
+  // Tách các trường đặc biệt ra khỏi query
+  const excludeFields = ["limit", "sort", "page", "fields"];
+  excludeFields.forEach((el) => delete queries[el]);
+
+  // Format lại các operators cho đúng cú pháp mongoose
+  let queryString = JSON.stringify(queries);
+  queryString = queryString.replace(
+    /\b(gte|gt|lt|lte)\b/g,
+    (macthedEl) => `$${macthedEl}`
+  );
+  const formatedQueries = JSON.parse(queryString);
+
+  // Filtering
+  if (queries?.title)
+    formatedQueries.title = { $regex: queries.title, $options: "i" };
+  let queryCommand = Product.find(formatedQueries);
+
+  // Sorting
+
+  // acb,efg => [abc,efg] => abc efg
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    queryCommand = queryCommand.sort(sortBy);
+  }
+
+  // Fields limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    queryCommand = queryCommand.select(fields);
+  }
+
+  // Pagination
+  // limit: số object lấy về 1 gọi API
+  // skip: 2
+  // 1 2 3 ... 10
+  // +2 => 2
+  // +dsdsad => NaN
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || process.env.LIMIT_PRODUCTS;
+  const skip = (page - 1) * limit;
+  queryCommand.skip(skip).limit(limit);
+  // Execute query
+  // Số lượng sp thỏa mãn điều kiện !== số lượng sp trả về 1 lần gọi API
+  const response = await queryCommand.exec();
+  const counts = await Product.find(formatedQueries).countDocuments();
+
   return res.status(200).json({
-    success: products ? true : false,
-    productDatas: products ? products : "Cannot get products",
+    success: response ? true : false,
+    counts,
+    products: response || "Cannot get products",
   });
+
+  // queryCommand.exec(async (err, response) => {
+  //   if (err) throw new Error(err.message);
+  //   const counts = await Product.find(formatedQueries).countDocuments();
+  //   return res.status(200).json({
+  //     success: response ? true : false,
+  //     counts,
+  //     products: response ? response : "Cannot get products",
+  //   });
+  // });
 });
 const updateProduct = asyncHandler(async (req, res) => {
   const { pid } = req.params;
@@ -46,11 +103,59 @@ const deleteProduct = asyncHandler(async (req, res) => {
     deletedProduct: deletedProduct ? deletedProduct : "Cannot delete product",
   });
 });
+const ratings = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, comment, pid } = req.body;
+  if (!star || !pid) throw new Error("Missing inputs");
+  const ratingProduct = await Product.findById(pid);
+  const alreadyRating = ratingProduct?.ratings?.find(
+    (el) => el.postedBy.toString() === _id
+  );
+  // console.log(alreadyRating);
+  if (alreadyRating) {
+    // update star & comment
+    await Product.updateOne(
+      {
+        ratings: { $elemMatch: alreadyRating },
+      },
+      {
+        $set: { "ratings.$.star": star, "ratings.$.comment": comment },
+      },
+      { new: true }
+    );
+  } else {
+    // add star & comment
+    await Product.findByIdAndUpdate(
+      pid,
+      {
+        $push: { ratings: { star, comment, postedBy: _id } },
+      },
+      { new: true }
+    );
+  }
 
+  // Sum ratings
+  const updatedProduct = await Product.findById(pid);
+  const ratingCount = updatedProduct.ratings.length;
+  const sumRatings = updatedProduct.ratings.reduce(
+    (sum, el) => sum + +el.star,
+    0
+  );
+  updatedProduct.totalRatings =
+    Math.round((sumRatings * 10) / ratingCount) / 10;
+
+  await updatedProduct.save();
+
+  return res.status(200).json({
+    status: true,
+    updatedProduct,
+  });
+});
 module.exports = {
   createProduct,
   getProduct,
   getProducts,
   updateProduct,
   deleteProduct,
+  ratings,
 };
