09d05944199b83360777346adf9c9ac10916e257
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-09-29 18:27:42 +0700

    #10 Header & Navigation

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="w-full">
+      <img
+        src="https://cdn.shopify.com/s/files/1/1903/4853/files/slideshow3-home2_1920x.jpg?v=1613166679"
+        alt="banner"
+        className="w-full object-contain"
+      />
+    </div>
+  );
 };
 
 export default Banner;
