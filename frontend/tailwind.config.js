718137d833cce37a3c1f7c7188f0c9d21977bb0d
MinhKhanhLA62 <nguyenminhkhanhla1.6@gmail.com>
2024-11-01 18:46:36 +0700

    #32 Danh sách sản phẩm

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -73,5 +73,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/forms")({ strategy: "class" }),
+  ],
 };
